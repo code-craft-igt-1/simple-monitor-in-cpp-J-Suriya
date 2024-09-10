604ab53f0744beca45ce59cf1530ff7be64396c9
Jayasuriya Selvaraj <Jayasuriya.S@philips.com>
2024-09-10 14:29:10 +0530

    Refactored functions to limit complexity

--- monitor.cpp
+++ monitor.cpp
@@ -1,18 +1,17 @@
-#include "./monitor.h"
-
+#include "monitor.h"
 #include <iostream>
-using std::cout;
+
 
 void alert(const char* message) {
-    cout << message << "\n";
+    std::cout << message << std::endl;
 }
 
 bool isTemperatureCritical(float temperature) {
-    return temperature > 102 || temperature < 95;
+    return (temperature < 36.0 || temperature > 37.5);
 }
 
 bool isPulseRateOutOfRange(float pulseRate) {
-    return pulseRate < 60 || pulseRate > 100;
+    return (pulseRate < 60 || pulseRate > 100);
 }
 
 bool isSpo2Low(float spo2) {
@@ -20,16 +19,8 @@ bool isSpo2Low(float spo2) {
 }
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-    if (isTemperatureCritical(temperature)) {
-        alert("Temperature is critical!");
-        return 0;
-    }
-    if (isPulseRateOutOfRange(pulseRate)) {
-        alert("Pulse Rate is out of range!");
-        return 0;
-    }
-    if (isSpo2Low(spo2)) {
-        alert("SpO2 level is low!");
+    if (isTemperatureCritical(temperature) || isPulseRateOutOfRange(pulseRate) || isSpo2Low(spo2)) {
+        alert("Vitals are not okay!");
         return 0;
     }
     return 1;
